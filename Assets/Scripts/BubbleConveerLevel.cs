f9cedb5a3ad740a69676cfa2b30dbfe9037ca4d7
kkkiraaa <karina.hmelyova@gmail.com>
2025-01-25 14:22:58 +0500

    Conveer
    Conveer

--- Assets/Scripts/BubbleConveerLevel.cs
+++ Assets/Scripts/BubbleConveerLevel.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 pointA = new Vector2(9, 2); // Начальная точка
+    public Vector2 pointB = new Vector2(0, 0); // Конечная точка
+    public float duration = 2.0f; // Время перемещения
 
-    // Update is called once per frame
-    void Update()
+    private float elapsedTime = 0f;
+
+    private void Update()
     {
-        
+        if (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration; // Нормализованное время
+            transform.position = Vector2.Lerp(pointA, pointB, t);
+        }
     }
 }
